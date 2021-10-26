303ca9c21d625e4ec7a54a171c69bf72f420850e
Gasper Vozel <gv@niteo.co>
2021-10-26 18:13:34 +0200

    Add mysql functions and tests

--- good/mysql.go
+++ good/mysql.go
@@ -2,6 +2,9 @@ package good
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -34,10 +37,20 @@ func GetMySQL(dataSource string) (MySQL, error) {
 	if err != nil {
 		return MySQL{}, err
 	}
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	db.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	db.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	db.SetConnMaxLifetime(time.Hour)
 	DB = MySQL{db}
 	return DB, nil
 }
 
+// GetCities returns a list of all cities in the `db`.
 func GetCities(db MySQL) (cities []City) {
 	res, err := db.Query("SELECT * FROM cities")
 
@@ -56,4 +69,58 @@ func GetCities(db MySQL) (cities []City) {
 		}
 	}
 	return
+}
+
+// AddCity inserts a City `city` to the `db` and returns it's new ID.
+func AddCity(db MySQL, city City) (int64, error) {
+
+	sql := fmt.Sprintf(
+		"INSERT INTO cities(name, population) VALUES ('%s', %d)",
+		city.Name, city.Population,
+	)
+	res, err := db.Exec(sql)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return res.LastInsertId()
+}
+
+// GetCityByID returns a `City` identified by ID `id`.
+func GetCityByID(db MySQL, id int64) (city City, err error) {
+	res, err := db.Query("SELECT * FROM cities WHERE id = ?", id)
+	defer res.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.Next() {
+		err := res.Scan(&city.Id, &city.Name, &city.Population)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return
+}
+
+// DeleteCityByIDs deletes `City`s which IDs are in `ids`
+func DeleteCityByIDs(db MySQL, ids ...int64) (affectedRows int64, err error) {
+	if len(ids) == 0 {
+		return 0, nil
+	}
+	sql := fmt.Sprintf("DELETE FROM cities WHERE id IN (%s)", sliceToString(ids, ", "))
+	res, err := db.Exec(sql)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res.RowsAffected()
+}
+
+func sliceToString(a []int64, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
--- good/mysql_test.go
+++ good/mysql_test.go
@@ -32,3 +32,70 @@ func TestGetCities(t *testing.T) {
 	}
 	assert.Equal(got, want)
 }
+
+func TestAddCity(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := GetMySQL(dockerMySQL)
+	got, err := AddCity(db, City{Name: "Ljubljana", Population: 200_000})
+	assert.NoError(err)
+	assert.Greater(got, int64(8))
+}
+
+func TestGetCityByID(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := GetMySQL(dockerMySQL)
+	got, err := GetCityByID(db, 8)
+	want := City{Id: 8, Name: "Berlin", Population: 3_671_000}
+	assert.NoError(err)
+	assert.Equal(got, want)
+}
+
+func TestDeleteCityByIDs(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := GetMySQL(dockerMySQL)
+
+	type args struct {
+		db  MySQL
+		ids []int64
+	}
+	tests := []struct {
+		name             string
+		args             args
+		wantAffectedRows int64
+		wantErr          error
+	}{
+		{name: "0 rows", args: args{db: db, ids: []int64{}}, wantAffectedRows: 0, wantErr: nil},
+		{name: "1 row", args: args{db: db, ids: []int64{1}}, wantAffectedRows: 1, wantErr: nil},
+		{name: "2 rows", args: args{db: db, ids: []int64{2, 3}}, wantAffectedRows: 2, wantErr: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotAffectedRows, err := DeleteCityByIDs(tt.args.db, tt.args.ids...)
+			assert.Equal(gotAffectedRows, tt.wantAffectedRows)
+			assert.Equal(err, tt.wantErr)
+		})
+	}
+}
+
+func Test_sliceToString(t *testing.T) {
+	assert := assert.New(t)
+	type args struct {
+		a     []int64
+		delim string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "0 elements", args: args{[]int64{}, ", "}, want: ""},
+		{name: "1 element", args: args{[]int64{1}, ", "}, want: "1"},
+		{name: "2 elements", args: args{[]int64{1, 2}, ", "}, want: "1, 2"},
+		{name: "3 elements", args: args{[]int64{1, 2, 3}, ", "}, want: "1, 2, 3"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(sliceToString(tt.args.a, tt.args.delim), tt.want)
+		})
+	}
+}
